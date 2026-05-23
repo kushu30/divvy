363b3f41d67403a651316b59e39656fc3911f34c
kushu30 <kushu123456789@gmail.com>
2026-05-24 00:30:53 +0530

    feat: setup Supabase client and auth screen

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://dhantcehzbmriksgvnek.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRoYW50Y2VoemJtcmlrc2d2bmVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1NTYzMzgsImV4cCI6MjA5NTEzMjMzOH0.tzkFBi7adUdefJ2r7ALIOe41WaLNEiVXxnJVPdhjJTY";
+const supabaseUrl =
+    process.env.EXPO_PUBLIC_SUPABASE_URL!;
+
+const supabaseAnonKey =
+    process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(
     supabaseUrl,
